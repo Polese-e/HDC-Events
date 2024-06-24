92064fae5b13cc1d533c2b45a28be149cb5c9c50
Polese-e <enzopolesedamasceno@gmail.com>
2024-06-24 19:23:27 -0300

    Advancing the Blade template engine

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,5 +25,21 @@
         @else
             <p>O nome não é pedro</p>
         @endif
+        <p>===========================</p>
+        @for($i = 0; $i < count($arr); $i++)
+            <p>Posição: {{ $i }} - Valor: {{ $arr[$i]}}</p>
+                @if ($i == 2)
+                    <p>O contador chegou a 3!</p>
+                @endif
+        @endfor
+        <p>===========================</p>
+        @foreach ($nomes as $nome)
+            <p>Posição: {{ $loop->index }} - Valor: {{ $nome }}</p>
+        @endforeach
+        <p>===========================</p>
+        @php
+            $name = "joão";
+            echo $name;
+        @endphp
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,10 @@ Route::get('/', function () {
     $nome = 'Matheus';
     $idade = '19';
     $trabalho = 'Programador';
+    $arr = [1,2,3,4,5];
+    $nomes = ['Sandra', 'Enzo', 'Gabriel', 'Igor'];
 
-    return view('welcome', ['nome' => $nome, 'idade2' => $idade, 'trabalho' => $trabalho]);
+    return view('welcome', ['nome' => $nome, 'idade2' => $idade, 'trabalho' => $trabalho, 'arr' => $arr, 'nomes' => $nomes]);
 });
 
 Route::get('/contatos', function () {
