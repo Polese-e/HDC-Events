5c3c5d283fa30377fe857316917700373e73aea0
Polese <enzopolese96@gmail.com>
2024-06-29 21:10:42 -0300

    Reset

---
+++ resources/views/produtos.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('title', 'Produtos')
+
+@section('content')
+<h1>Tela de Produtos</h1>
+@if ($query != '')
+    <p>O usuário está buscando por {{ $query }}</p>
+@endif
+<a href="/">Retornar ao Início</a>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [EventController::class, 'index']);
+Route::get('/events/create', [EventController::class, 'create']);
+
+Route::get('/contatos', [ContactController::class, 'index']);
+
+Route::get('/produtos', [ProductController::class, 'index']);
+
+Route::get('/produto_teste/{id?}', [ProductController::class, 'test']);
