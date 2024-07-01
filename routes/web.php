f26209fbd0e8489f69c023a27384f03ee1c46183
Polese <enzopolese96@gmail.com>
2024-07-01 17:54:06 -0300

    Home HTML

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,5 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [EventController::class, 'index']);
-
 Route::get('/events/create', [EventController::class, 'create']);
-
 Route::get('/contatos', [ContactController::class, 'index']);
