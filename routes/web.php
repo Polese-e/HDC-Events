985e4daa0653e1644d540e5ab1d59296a4dcc9be
Polese <enzopolesedamasceno@gmail.com>
2024-07-08 18:37:15 -0300

    Rescuing a record from the database

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create']);
+Route::get('/events/{id}', [EventController::class, 'show']);
 Route::post('/events', [EventController::class, 'store']);
